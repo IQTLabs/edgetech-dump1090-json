9d71331c27bfdb568bbc707d2126c48d2d706510
Raymond LeClair <raymond.leclair@springbok.io>
2023-08-17 12:50:56 -0400

    Remove config handling

--- dump1090-json/dump1090-json_pub_sub.py
+++ dump1090-json/dump1090-json_pub_sub.py
@@ -50,7 +50,6 @@ class Dump1090PubSub(BaseMQTTPubSub):
         self: Any,
         dump1090_host: str,
         dump1090_http_port: str,
-        config_json_topic: str,
         ads_b_json_topic: str,
         continue_on_exception: bool = False,
         **kwargs: Any,
@@ -61,8 +60,6 @@ class Dump1090PubSub(BaseMQTTPubSub):
         Args:
             dump1090_host (str): Host IP of the dump1090 system
             dump1090_port (str): Host port of the dump1090 socket
-            config_json_topic (str): MQTT topic for subscribing to config
-                messages
             ads_b_json_topic (str): MQTT topic to publish the data from
                 the port to. Specified via docker-compose.
             continue_on_exception (bool): Continue on unhandled
@@ -71,7 +68,6 @@ class Dump1090PubSub(BaseMQTTPubSub):
         super().__init__(**kwargs)
         self.dump1090_host = dump1090_host
         self.dump1090_http_port = dump1090_http_port
-        self.config_json_topic = config_json_topic
         self.ads_b_json_topic = ads_b_json_topic
         self.continue_on_exception = continue_on_exception
 
@@ -81,7 +77,14 @@ class Dump1090PubSub(BaseMQTTPubSub):
         self.publish_registration("Dump1090 Sender Registration")
 
         # Log configuration parameters
-        self._log_config()
+        logging.info(
+            f"""Dump1090PubSub initialized with parameters:
+    dump1090_host = {dump1090_host}
+    dump1090_http_port = {dump1090_http_port}
+    ads_b_json_topic = {ads_b_json_topic}
+    continue_on_exception = {continue_on_exception}
+            """
+        )
 
     def decode_payload(
         self, msg: Union[mqtt.MQTTMessage, str], data_payload_type: str
@@ -108,58 +111,6 @@ class Dump1090PubSub(BaseMQTTPubSub):
         data_payload = json.loads(payload)[data_payload_type]
         return json.loads(data_payload)
 
-    def _config_callback(
-        self,
-        _client: Union[mqtt.Client, None],
-        _userdata: Union[Dict[Any, Any], None],
-        msg: Union[mqtt.MQTTMessage, Dict[Any, Any]],
-    ) -> None:
-        """
-        Process config message.
-
-        Parameters
-        ----------
-        _client: Union[mqtt.Client, None]
-            MQTT client
-        _userdata: Union[Dict[Any, Any], None]
-            Any required user data
-        msg: Union[mqtt.MQTTMessage, Dict[Any, Any]]
-            An MQTT message, or dictionary
-
-        Returns
-        -------
-        None
-        """
-        # Assign data attributes allowed to change during operation,
-        # ignoring config message data without a "dump1090-json" key
-        data = self.decode_payload(msg, "Configuration")
-        if "dump1090-json" not in data:
-            return
-        logging.info(f"Processing config message data: {data}")
-        config = data["dump1090-json"]
-        self.dump1090_host = config.get("dump1090_host", self.dump1090_host)
-        self.dump1090_http_port = config.get(
-            "dump1090_http_port", self.dump1090_http_port
-        )
-        self.config_json_topic = config.get("config_json_topic", self.config_json_topic)
-        self.ads_b_json_topic = config.get("ads_b_json_topic", self.ads_b_json_topic)
-        self.continue_on_exception = config.get(
-            "continue_on_exception", self.continue_on_exception
-        )
-
-        # Log configuration parameters
-        self._log_config()
-
-    def _log_config(self: Any) -> None:
-        """Logs all paramters that can be set on construction."""
-        config = {
-            "dump1090_host": self.dump1090_host,
-            "dump1090_http_port": self.dump1090_http_port,
-            "config_json_topic": self.config_json_topic,
-            "ads_b_json_topic": self.ads_b_json_topic,
-            "continue_on_exception": self.continue_on_exception,
-        }
-        logging.info(f"Dump1090PubSub configuration:\n{json.dumps(config, indent=4)}")
 
     def _process_response(self) -> None:
         """Process the response from the Dump1090 aircraft endpoint,
@@ -284,9 +235,6 @@ class Dump1090PubSub(BaseMQTTPubSub):
         )
         schedule.every(1).seconds.do(self._process_response)
 
-        # Subscribe to required topics
-        self.add_subscribe_topic(self.config_json_topic, self._config_callback)
-
         logging.info("System initialized and running")
         while True:
             try:
@@ -313,7 +261,6 @@ def make_dump1090() -> Dump1090PubSub:
         mqtt_ip=os.environ.get("MQTT_IP", ""),
         dump1090_host=os.environ.get("DUMP1090_HOST", ""),
         dump1090_http_port=os.environ.get("DUMP1090_HTTP_PORT", ""),
-        config_json_topic=os.getenv("CONFIG_JSON_TOPIC", ""),
         ads_b_json_topic=os.getenv("ADS_B_JSON_TOPIC", ""),
         continue_on_exception=ast.literal_eval(
             os.environ.get("CONTINUE_ON_EXCEPTION", "False")
